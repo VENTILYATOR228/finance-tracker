6947586aca477b6bf9c46b47526b1113ef1aa976
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-25 09:48:06 +0300

    Add first & last name fields to edit user page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permited_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
