d59999e913ba704603f353d659c01590f754752f
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-25 09:57:31 +0300

    Add first & last names during signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
