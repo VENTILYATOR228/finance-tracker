e5b5a82d5b13dc3725d26200890810d20d91401d
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-25 15:37:39 +0300

    Add making friends functionality on the friends page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,20 @@
 class FriendshipsController < ApplicationController
 
   def create
-    
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You are following #{friend.full_name}"
+    else
+      flash[:alert] = "Something's gone wrong"
+    end
+    redirect_to friends_path
   end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
-    flash[:notice] = "#{friend.full_name} was successfully removed from your friendlist"
+    flash[:notice] = "Stopped following"
     redirect_to friends_path
   end
 
