e428c688680379ccf52ec6bb760069c7b5ac5133
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-25 16:36:20 +0300

    Display user/friend profile & adding tracking options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show]
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
-  devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'friends', to: 'users#friend_list'
